783734049e3ffc6e31c2715fad81b97c64612ca0
Jing Li <406539873@qq.com>
2016-02-14 01:02:32 -0500

    Merge branch 'master' into jingLi
    # Conflicts:
    #	test.java

--- test.java
+++ test.java
@@ -1,8 +1,11 @@
 public class test {
 	public static void main(String[] args){
 		System.out.println("Hello World");
+		System.out.println("Hello World");
         int i;
         i++;
         System.out.println(i);
 	}
+
+
 }
